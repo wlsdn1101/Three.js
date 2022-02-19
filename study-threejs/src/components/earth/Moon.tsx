32804c2e23a44f382711c54412e497d2d35442fa
LeeJinWoo <wlsdn5793@gmail.com>
2022-02-19 18:52:50 +0900

    ⚡️ :: 달의 자전
    달의 공전은 포기함

--- study-threejs/src/components/earth/Moon.tsx
+++ study-threejs/src/components/earth/Moon.tsx
@@ -1,12 +1,19 @@
 import React, { useRef } from "react";
 import { TextureLoader } from "three";
-import { useLoader } from "react-three-fiber";
+import { useFrame, useLoader } from "react-three-fiber";
+import * as THREE from "three";
 import MoonMap from "../../assets/Moon.jpg";
 const Moon = () => {
   const moonMap = useLoader(TextureLoader, MoonMap);
-  const ref = useRef();
+  const moonRef = useRef<any>();
+  const moonPosition = new THREE.Vector3(2, 2, 2);
+
+  useFrame(({ clock }) => {
+    const time = clock.getElapsedTime();
+    moonRef.current.rotation.y = time / 8;
+  });
   return (
-    <mesh scale={0.5} position={[2, 2, 2]} ref={ref}>
+    <mesh scale={0.5} position={moonPosition} ref={moonRef}>
       <sphereGeometry args={[1, 32, 32]}></sphereGeometry>
       <meshStandardMaterial map={moonMap} />
     </mesh>
